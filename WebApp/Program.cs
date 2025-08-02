2949233c1df6b1d92cdde41baf5c56c56f9b1528
Trevor Takawira <trevortaks@gmail.com>
2025-08-02 19:05:16 +0000

    Simplify Blazor app and connect to API

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using HospitalManagementSystem.Infrastructure.Data;
-using HospitalManagementSystem.Infrastructure.Identity;
 using HospitalManagementSystem.WebApp.Services;
-using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,83 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 
-// Database
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Identity
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
-
-// Authentication State
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
-
-// MudBlazor
-builder.Services.AddMudServices(config =>
-{
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
-    config.SnackbarConfiguration.PreventDuplicates = false;
-    config.SnackbarConfiguration.NewestOnTop = true;
-    config.SnackbarConfiguration.ShowCloseIcon = true;
-    config.SnackbarConfiguration.VisibleStateDuration = 10000;
-    config.SnackbarConfiguration.ShowTransitionDuration = 300;
-    config.SnackbarConfiguration.HideTransitionDuration = 500;
-});
-
-// Application Services
+// Register HTTP client for API calls
 builder.Services.AddHttpClient<IPatientService, PatientService>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
+    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/");
 });
 
-builder.Services.AddHttpClient<IDoctorService, DoctorService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
-});
-
-builder.Services.AddHttpClient<IMedicalRecordService, MedicalRecordService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
-});
-
-builder.Services.AddHttpClient<IAppointmentService, AppointmentService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
-});
-
-builder.Services.AddHttpClient<IBillingService, BillingService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
-});
-
-builder.Services.AddHttpClient<IAnalyticsService, AnalyticsService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
-});
-
-builder.Services.AddHttpClient<IAuthService, AuthService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
-});
-
-builder.Services.AddScoped<INotificationService, NotificationService>();
-builder.Services.AddScoped<RealTimeService>();
-
-// HTTP Client with Authentication
-builder.Services.AddScoped(sp =>
-{
-    var clientFactory = sp.GetRequiredService<IHttpClientFactory>();
-    var client = clientFactory.CreateClient();
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7123/api/");
-    return client;
-});
-
-builder.Services.AddApplication();
-// AutoMapper
-builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -102,22 +27,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-// Initialize database
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    context.Database.Migrate();
-    
-    var initializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
-    await initializer.InitializeAsync();
-}
-
 app.Run();
-
 
